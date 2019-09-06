f9027a04346329d924487f53a2ede207fd537162
maxim <dzy91maxim@gmail.com>
2019-09-06 10:35:42 +0600

    fix(edu/CmsCreateSlider): Patch data insert product

--- app/code/Edu/CmsCreateSlider/Setup/Patch/Data/AddNewCmsProducts.php
+++ app/code/Edu/CmsCreateSlider/Setup/Patch/Data/AddNewCmsProducts.php
@@ -23,15 +23,23 @@ class AddNewCmsProducts implements
     protected $moduleDataSetup;
 
     /**
-     * @param ProductFactory $productFactory
+     * @var \Magento\Framework\App\State
+     */
+    private $state;
+
+    /**
      * @param ModuleDataSetupInterface $moduleDataSetup
+     * @param ProductFactory $productFactory
+     * @param \Magento\Framework\App\State $state
      */
     public function __construct(
         ModuleDataSetupInterface $moduleDataSetup,
-        ProductFactory $productFactory
+        ProductFactory $productFactory,
+        \Magento\Framework\App\State $state
     ) {
         $this->moduleDataSetup = $moduleDataSetup;
         $this->productFactory = $productFactory;
+        $this->state = $state;
     }
 
     /**
@@ -41,19 +49,25 @@ class AddNewCmsProducts implements
     public function apply()
     {
         $this->moduleDataSetup->startSetup();
-        #$this->productFactory->create()->setData($productData)->save();
-
-        $simpleProduct1 = $this->productFactory->create();
-        $simpleProduct1->setData('sku', 'Test Simple Product 1');
-        $simpleProduct1->setData('name', 'Test Simple Product 1');
-        $simpleProduct1->setData('website_ids', [1]); // product can be found in main website
-        $simpleProduct1->setData('status', \Magento\Catalog\Model\Product\Attribute\Source\Status::STATUS_ENABLED);
-        $simpleProduct1->setData('visibility', 4);
-        $simpleProduct1->setData('price', 12);
-        $simpleProduct1->setData('type_id', 'simple');
-
-        $simpleProduct1->save();
-
+        $this->state->setAreaCode(\Magento\Framework\App\Area::AREA_FRONTEND);
+        for ($i = 5; $i <= 15; $i++) {
+            $simpleProduct1 = $this->productFactory->create();
+            $simpleProduct1->setData('sku', 'Simple Product ' . $i);
+            $simpleProduct1->setData('name', 'Simple Product ' . $i);
+            $simpleProduct1->setData('attribute_set_id', '4');
+            $simpleProduct1->setData('website_ids', [1]); // product can be found in main website
+            $simpleProduct1->setData('status', \Magento\Catalog\Model\Product\Attribute\Source\Status::STATUS_ENABLED);
+            $simpleProduct1->setData('visibility', 4);
+            $simpleProduct1->setData('price', 12);
+            $simpleProduct1->setData('type_id', 'simple');
+            $simpleProduct1->setData('stock_data', [
+                'use_config_manage_stock' => 0,
+                'manage_stock' => 1,
+                'is_in_stock' => 1,
+                'qty' => 100
+            ]);
+            $simpleProduct1->save();
+        }
         $this->moduleDataSetup->endSetup();
     }
 

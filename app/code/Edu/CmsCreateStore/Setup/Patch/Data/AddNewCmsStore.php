969831ca38dfa5b2dcfeb6a8d5c531703868703d
maxim <dzy91maxim@gmail.com>
2019-09-11 11:20:24 +0600

    Refactored(edu/CmsCreateStore) - add function create store - fix namespace CmsCreateStore

--- app/code/Edu/CmsCreateStore/Setup/Patch/Data/AddNewCmsStore.php
+++ app/code/Edu/CmsCreateStore/Setup/Patch/Data/AddNewCmsStore.php
@@ -1,13 +1,10 @@
 <?php
 
-namespace Edu\CmsCreateSlider\Setup\Patch\Data;
+namespace Edu\CmsCreateStore\Setup\Patch\Data;
 
 use Magento\Framework\Setup\ModuleDataSetupInterface;
 use Magento\Framework\Setup\Patch\DataPatchInterface;
 use Magento\Store\Model\StoreFactory;
-use Magento\Store\Model\StoreRepository;
-use Magento\Store\Model\StoreManagerInterface;
-
 /**
  * Class AddNewCmsPageTask2
  * @package Edu\CmsCreateSlider\Setup\Patch\Data
@@ -19,36 +16,24 @@ class AddNewCmsStore implements
      * @var StoreFactory
      */
     protected $storeFactory;
-    /**
-     * @var StoreRepository
-     */
-    protected $storeRepository;
+
     /**
      * @var ModuleDataSetupInterface
      */
     protected $moduleDataSetup;
 
-    /**
-     * @var StoreManagerInterface
-     */
-    protected $storeManager;
+
 
     /**
      * @param ModuleDataSetupInterface $moduleDataSetup
      * @param StoreFactory $storeFactory
-     * @param StoreRepository $storeRepository
-     * @param StoreManagerInterface $storeManager
      */
     public function __construct(
         ModuleDataSetupInterface $moduleDataSetup,
-        StoreFactory $storeFactory,
-        StoreRepository $storeRepository,
-        StoreManagerInterface $storeManager
+        StoreFactory $storeFactory
     ) {
         $this->moduleDataSetup = $moduleDataSetup;
         $this->storeFactory = $storeFactory;
-        $this->storeRepository = $storeRepository;
-        $this->storeManager = $storeManager;
     }
 
     /**
@@ -58,36 +43,16 @@ class AddNewCmsStore implements
     public function apply()
     {
         $this->moduleDataSetup->startSetup();
-        $storeManager = $this->storeManager->getStore();
-        //$store=createOrUpdateStore();
-        //$page = $this->pageFactory->create()->setData($pageData);
-        //$this->pageRepository->save($page);
-        $this->moduleDataSetup->endSetup();
-    }
-
-    /**
-     * @param $group
-     * @param $websiteId
-     * @param $storeViewName
-     * @param $storeViewCode
-     * @param $data
-     * @return mixed
-     * @throws \Magento\Framework\Exception\LocalizedException
-     */
-    private function createOrUpdateStore($group, $websiteId, $storeViewName, $storeViewCode, $data)
-    {
         $store = $this->storeFactory->create();
-        if ($group->getId() && $group->getDefaultStoreId()) {
-            $store->load($group->getDefaultStoreId());
-        }
-        $store->setName($storeViewName . ' - Store View');
-        $store->setCode($storeViewCode);
-        $store->setWebsiteId($websiteId);
-        $store->setGroupId($group->getId());
-        $store->setSortOrder($data['sort_order']);
-        $store->setIsActive($data['is_active']);
-        $create_store = $this->storeRepository->save($store);
-        return $create_store;
+        $store->setName('USD');
+        $store->setCode("en");
+        $store->setWebsiteId(1);
+        $store->setGroupId(1);
+        $store->setSortOrder(1);
+        $store->setIsActive(1);
+        $store->save();
+        //$create_store = $this->storeRepository->save($store);
+        $this->moduleDataSetup->endSetup();
     }
 
     /**

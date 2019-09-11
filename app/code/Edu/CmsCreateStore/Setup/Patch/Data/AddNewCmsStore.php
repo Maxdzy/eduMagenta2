c5fd358d0effb7f314e370f64a9e221f92bff01b
maxim <dzy91maxim@gmail.com>
2019-09-11 16:17:50 +0600

    Refactored(edu/CmsCreateStore) - change config store

--- app/code/Edu/CmsCreateStore/Setup/Patch/Data/AddNewCmsStore.php
+++ app/code/Edu/CmsCreateStore/Setup/Patch/Data/AddNewCmsStore.php
@@ -2,9 +2,13 @@
 
 namespace Edu\CmsCreateStore\Setup\Patch\Data;
 
+use Magento\Config\Model\ConfigFactory;
+use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
 use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Store\Model\ResourceModel\Store as StoreResource;
 use Magento\Store\Model\StoreFactory;
+
 /**
  * Class AddNewCmsPageTask2
  * @package Edu\CmsCreateSlider\Setup\Patch\Data
@@ -16,24 +20,37 @@ class AddNewCmsStore implements
      * @var StoreFactory
      */
     protected $storeFactory;
+    /**
+     * @var ConfigFactory
+     */
+    protected $configFactory;
+
+    /**
+     * @var StoreResource
+     */
+    protected $storeResource;
 
     /**
      * @var ModuleDataSetupInterface
      */
     protected $moduleDataSetup;
 
-
-
     /**
      * @param ModuleDataSetupInterface $moduleDataSetup
      * @param StoreFactory $storeFactory
+     * @param StoreResource $storeResource
+     * @param ConfigFactory $configFactory
      */
     public function __construct(
         ModuleDataSetupInterface $moduleDataSetup,
-        StoreFactory $storeFactory
+        StoreFactory $storeFactory,
+        StoreResource $storeResource,
+        ConfigFactory $configFactory
     ) {
         $this->moduleDataSetup = $moduleDataSetup;
+        $this->storeResource = $storeResource;
         $this->storeFactory = $storeFactory;
+        $this->configFactory = $configFactory;
     }
 
     /**
@@ -42,16 +59,33 @@ class AddNewCmsStore implements
      */
     public function apply()
     {
+        $storeData = [
+            'setName' => 'USD',
+            'setCode' => "en",
+            'setWebsiteId' => 1,
+            'setGroupId' => 1,
+            'setSortOrder' => 1,
+            'setIsActive' => 1,
+        ];
         $this->moduleDataSetup->startSetup();
-        $store = $this->storeFactory->create();
-        $store->setName('USD');
-        $store->setCode("en");
-        $store->setWebsiteId(1);
-        $store->setGroupId(1);
-        $store->setSortOrder(1);
-        $store->setIsActive(1);
-        $store->save();
-        //$create_store = $this->storeRepository->save($store);
+        //$store = $this->storeFactory->create()->setData($storeData);
+        //$this->storeResource->save($store);
+
+        $config = [
+            'scope' => "stores",
+            'scopeId' => 5,
+            'path' => "catalog/seo/product_url_suffix",
+            'value' => "",
+        ];
+
+        $configModel = $this->configFactory->create();
+        $configModel->setWebsite($config['scopeId']);
+        $configModel->setStore($config['scopeId']);
+        //$config->setScope(ScopeConfigInterface::SCOPE_TYPE_DEFAULT);
+
+        $configModel->setDataByPath($config['path'], $config['value']);
+        $configModel->save();
+
         $this->moduleDataSetup->endSetup();
     }
 

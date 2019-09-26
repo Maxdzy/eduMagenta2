51111cfc7ac49e0cdb903630f46080ae878de8e4
maxim <dzy91maxim@gmail.com>
2019-09-26 11:13:50 +0600

    feat(Edu/CmsSimpleBadge) - add BadgesInterface - edit Badges item

---
+++ app/code/Edu/CmsSimpleBadge/Api/Data/BadgesInterface.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * @category    Edu
+ * @package     Edu\CmsSimpleBadge
+ * @author      Maxim Dzyuba
+ */
+
+namespace Edu\CmsSimpleBadge\Api\Data;
+
+use Zend\Db\Sql\Ddl\Column\Varchar;
+
+interface BadgesInterface
+{
+    /**
+     * Constants for keys of data array. Identical to the name of the getter in snake case.
+     */
+    const BADGE_ID = 'badge_id';
+    const NAME = 'name';
+    const IMAGE_URL = 'image_url';
+    const STATUS = 'status';
+    const CREATED_AT = 'created_at';
+    const UPDATE_TIME = 'update_time';
+
+    /**
+     * Get BadgeId.
+     *
+     * @return int
+     */
+    public function getBadgeId();
+
+    /**
+     * Set BadgeId.
+     */
+    public function setBadgeId($badgeId);
+
+    /**
+     * Get Name.
+     *
+     * @return varchar
+     */
+    public function getName();
+
+    /**
+     * Set Name.
+     */
+    public function setName($name);
+
+    /**
+     * Get getImage_url.
+     *
+     * @return varchar
+     */
+    public function getImageUrl();
+
+    /**
+     * Set Image_url.
+     */
+    public function setImageUrl($imageUrl);
+
+    /**
+     * Get Status.
+     *
+     * @return varchar
+     */
+    public function getStatus();
+
+    /**
+     * Set Status.
+     */
+    public function setStatus($status);
+
+    /**
+     * Get CreatedAt.
+     *
+     * @return varchar
+     */
+    public function getCreatedAt();
+
+    /**
+     * Set CreatedAt.
+     */
+    public function setCreatedAt($createdAt);
+
+    /**
+     * Get UpdateTime.
+     *
+     * @return varchar
+     */
+    public function getUpdateTime();
+
+    /**
+     * Set UpdateTime.
+     */
+    public function setUpdateTime($updateTime);
+}
--- app/code/Edu/CmsSimpleBadge/Model/Badges.php
+++ app/code/Edu/CmsSimpleBadge/Model/Badges.php
@@ -8,30 +8,142 @@
 namespace Edu\CmsSimpleBadge\Model;
 
 use Magento\Framework\Model\AbstractModel;
-use Magento\Framework\DataObject\IdentityInterface;
+use Edu\CmsSimpleBadge\Api\Data\BadgesInterface;
+use Zend\Db\Sql\Ddl\Column\Varchar;
 
-class Badges extends AbstractModel implements IdentityInterface
+class Badges extends AbstractModel implements BadgesInterface
 {
+    /**
+     * CMS page cache tag.
+     */
     const CACHE_TAG = 'edu_cmssimplebadge_badges';
 
+    /**
+     * @var string
+     */
     protected $_cacheTag = 'edu_cmssimplebadge_badges';
 
+    /**
+     * Prefix of model events names.
+     *
+     * @var string
+     */
     protected $_eventPrefix = 'edu_cmssimplebadge_badges';
 
+    /**
+     * Initialize resource model.
+     */
     protected function _construct()
     {
         $this->_init('Edu\CmsSimpleBadge\Model\ResourceModel\Badges');
     }
 
-    public function getIdentities()
+    /**
+     * Get BadgeId.
+     *
+     * @return int
+     */
+    public function getBadgeId()
     {
-        return [self::CACHE_TAG . '_' . $this->getId()];
+        return $this->getData(self::BADGE_ID);
     }
 
-    public function getDefaultValues()
+    /**
+     * Set BadgeId.
+     */
+    public function setBadgeId($badgeId)
     {
-        $values = [];
-
-        return $values;
+        return $this->setData(self::BADGE_ID, $badgeId);
     }
+
+    /**
+     * Get Name.
+     *
+     * @return varchar
+     */
+    public function getName()
+    {
+        return $this->getData(self::NAME);
+    }
+
+    /**
+     * Set Name.
+     */
+    public function setName($name)
+    {
+        return $this->setData(self::NAME, $name);
+    }
+
+    /**
+     * Get getImage_url.
+     *
+     * @return varchar
+     */
+    public function getImageUrl()
+    {
+        return $this->getData(self::IMAGE_URL);
+    }
+
+    /**
+     * Set Image_url.
+     */
+    public function setImageUrl($imageUrl)
+    {
+        return $this->setData(self::IMAGE_URL, $imageUrl);
+    }
+
+    /**
+     * Get Status.
+     *
+     * @return varchar
+     */
+    public function getStatus()
+    {
+        return $this->getData(self::STATUS);
+    }
+
+    /**
+     * Set Status.
+     */
+    public function setStatus($status)
+    {
+        return $this->setData(self::STATUS, $status);
+    }
+
+    /**
+     * Get CreatedAt.
+     *
+     * @return varchar
+     */
+    public function getCreatedAt()
+    {
+        return $this->getData(self::CREATED_AT);
+    }
+
+    /**
+     * Set CreatedAt.
+     */
+    public function setCreatedAt($createdAt)
+    {
+        return $this->setData(self::CREATED_AT, $createdAt);
+    }
+
+    /**
+     * Get UpdateTime.
+     *
+     * @return varchar
+     */
+    public function getUpdateTime()
+    {
+        return $this->getData(self::UPDATE_TIME);
+    }
+
+    /**
+     * Set UpdateTime.
+     */
+    public function setUpdateTime($updateTime)
+    {
+        return $this->setData(self::UPDATE_TIME, $updateTime);
+    }
+
 }
